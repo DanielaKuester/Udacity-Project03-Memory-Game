a98fc7ab38727cfa8303aeb62add663772f79a9a
Daniela Küster <daniela.kuester@gmx.net>
2018-06-04 22:55:08 +0200

    Refactor: Move openCards/pushCards functions

--- js/app.js
+++ js/app.js
@@ -136,39 +136,36 @@ function startGame() {
 			function showCards() {
 
 				cards[i].addEventListener('click', function (e) {
-
-					function openCards() {
-						//console.log("This card was clicked!")
-						cards[i].classList.add("open", "show");
-						openedCards++;
-						console.log("opened cards: " + openedCards);
-					}
 					openCards();
-
-					function pushCards() {
-						if (openedCards > 2) {
-							// Print an alarm if more than two items are in the array of flipped cards
-							console.log("%c Alert - Too many items in array!!!",  "color: white; background: blue");
-						}
-
-						if (flippedCards.length < 2) {
-							flippedCards.push(cards[i]);
-							console.log("Length of array: " + flippedCards.length);
-							console.log("Items in flippedCards: " + flippedCards);
-						}
-
-						if (flippedCards.length === 1) {
-							cards[i].classList.add("disableClick");
-						}
-						if (flippedCards.length === 2) {
-							cardOne = flippedCards[0];
-							cardTwo = flippedCards[1];
-							pairCards();
-						}
-					}
 					pushCards();
 				})
+				function openCards() {
+					//console.log("This card was clicked!")
+					cards[i].classList.add("open", "show");
+					openedCards++;
+					console.log("opened cards: " + openedCards);
+				}
+				function pushCards() {
+					if (openedCards > 2) {
+						// Print an alarm if more than two items are in the array of flipped cards
+						console.log("%c Alert - Too many items in array!!!",  "color: white; background: blue");
+					}
 
+					if (flippedCards.length < 2) {
+						flippedCards.push(cards[i]);
+						console.log("Length of array: " + flippedCards.length);
+						console.log("Items in flippedCards: " + flippedCards);
+					}
+
+					if (flippedCards.length === 1) {
+						cards[i].classList.add("disableClick");
+					}
+					if (flippedCards.length === 2) {
+						cardOne = flippedCards[0];
+						cardTwo = flippedCards[1];
+						pairCards();
+					}
+				}
 			}
 			showCards();
 		}
